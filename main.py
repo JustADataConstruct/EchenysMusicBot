4159d1b4a6ad4f832d7d8af2bc81ef4f73092299
JustADataConstruct <yarukatsaros@gmail.com>
2021-01-23 00:47:01 +0100

    Bot can now join and leave voice channels.

--- main.py
+++ main.py
@@ -2,25 +2,50 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-description = "Music bot for the GCCM server"
+description = "Music bot for the Echenys server"
 
-bot = commands.Bot('/',description=description,intents=intents)
+bot = commands.Bot('!',description=description,intents=intents)
 
 token = ""
 try:
     with open('token.txt') as f:
         token = f.read()
 except:
-    print("Can't find token file!");
+    print("Can't find token file!")
 
 @bot.event
 async def on_ready():
     print("Connected")
 
+
+@bot.command(name="alive", description="Check if I am working correctly.")
+async def ping(ctx):
+    await ctx.send("I live, father.")
+
+@bot.command(name="join")
+async def join(ctx):
+    if ctx.author.voice and ctx.author.voice.channel:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+        await ctx.send(f"Connected to {channel.name}")
+    else:
+        await ctx.send("You aren't in a voice channel.")
+
+@bot.command(name="leave")
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+    else:
+        await ctx.send("I am not in voice channel.")
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-    await message.channel.send("I hear you.")
+    if message.content.lower() == "hello":
+        await message.channel.send("Hi!")
+        return
+    await bot.process_commands(message)
+
 
 bot.run(token)
