e0266244caf9c4722c74b11415333574d13130ad
JustADataConstruct <yarukatsaros@gmail.com>
2021-01-23 12:37:06 +0100

    Songs can now define an start and end point.

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ async def on_ready():
     print("Connected")
 
 songs = {}
+songqueue = []
+
 try:
     with open('songs.json') as f: #TODO: Would a database work better? Probably yes.
         s = f.read()
@@ -49,22 +51,67 @@ async def leave(ctx):
     else:
         await ctx.send("I am not in voice channel.")
 
-#TODO: Playlists. (Indicate the name of a json file with all the songs?)
-
 @bot.command(name="play")
 async def play(ctx,name:str):
     try:
         if ctx.voice_client:
             if ctx.voice_client.is_playing():
                 ctx.voice_client.stop()
-            ctx.voice_client.play(discord.FFmpegPCMAudio(songs[name.lower()]),after=lambda e:print('done',e))
+            options= ""
+            song = songs[name.lower()]
+            route = song["route"]
+            if 'start_point' in song:
+                sp = song["start_point"]
+                options += f"-ss {sp}"
+            if 'end_point' in song:
+                ep = song["end_point"]
+                options += f" -to {ep}"
+            ctx.voice_client.play(discord.FFmpegPCMAudio(route,options=options),after=lambda e:print('done',e))
             await ctx.send(f"Playing {name}")
-    except Exception:
+    except Exception as e:
         await ctx.send("Song not found.")
+        print(e)
+
+@bot.command(name="playlist")
+async def playlist(ctx,name:str):
+    try:
+        for song_object in songs[name.lower()]:
+            songqueue.append(song_object)
+        if not ctx.voice_client.is_playing():
+            options = ""
+            song = songqueue[0]
+            route = song["route"]
+            if 'start_point' in song:
+                sp = song["start_point"]
+                options += f"-ss {sp}"
+            if 'end_point' in song:
+                ep = song["end_point"]
+                options += f" -to {ep}"            
+            ctx.voice_client.play(discord.FFmpegPCMAudio(route,options=options),after=lambda e:play_next(ctx))
+    except Exception as e:
+        print(e)
+
+def play_next(ctx):
+    if len(songqueue) == 0: return    
+    del songqueue[0]
+    if len(songqueue) >= 1:
+        options = ""
+        song = songqueue[0]
+        route = song["route"]
+        if 'start_point' in song:
+            sp = song["start_point"]
+            options += f"-ss {sp}"
+        if 'end_point' in song:
+            ep = song["end_point"]
+            options += f" -to {ep}"            
+        ctx.voice_client.play(discord.FFmpegPCMAudio(route,options=options),after=lambda e:play_next(ctx))
+    else:
+        print("end of playlist.")
 
 @bot.command()
 async def stop(ctx):
     try:
+        songqueue.clear()
         ctx.voice_client.stop()
     except:
         await ctx.send("Not playing anything.")
@@ -82,6 +129,13 @@ async def resume(ctx):
         ctx.voice_client.resume()
     except:
         await ctx.send("Can't resume")
+
+@bot.command()
+async def skip(ctx):
+    try:
+        ctx.voice_client.stop()
+    except Exception as e:
+        print(e)
 
 # @bot.event
 # async def on_message(message):
