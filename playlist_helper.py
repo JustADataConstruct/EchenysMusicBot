bfa847d1d10936f0bb58352907d4a0f707835cfb
JustADataConstruct <yarukatsaros@gmail.com>
2021-01-28 16:27:35 +0100

    Helper script: error handling.

--- playlist_helper.py
+++ playlist_helper.py
@@ -1,8 +1,7 @@
-import sys
 import json
+import sys
 from os import listdir
-from os.path import isfile,join,abspath
-from glob import glob
+from os.path import abspath, isfile, join
 
 # A helper script to quickly generate playlists. Note that it's a very basic script: it doesn't preserve any kind of
 # file order, nor can start or end times be indicated unless the output file is manually edited. It's useful mostly
@@ -14,7 +13,6 @@ def print_usage():
     print("Note: this script will grab every file on the indicated folder.")
     print("Make sure the folder only contains the audio files you want to add to the playlist.")
 
-#TODO: Error handling.
 if len(sys.argv) < 3 :
     print_usage()
     sys.exit()
@@ -23,10 +21,14 @@ folder = sys.argv[1]
 name = sys.argv[2]
 playlist = []
 
-
-files = [f for f in listdir(folder) if isfile(join(folder,f))]
-for m in files:
-    playlist.append(abspath(join(folder,m)))
+try:
+    files = [f for f in listdir(folder) if isfile(join(folder,f))]
+    for m in files:
+        playlist.append(abspath(join(folder,m)))
+except Exception as e:
+    print("Something went wrong trying to read from the indicated folder.")
+    print(e)
+    sys.exit()
 
 olist = []
 for m in playlist:
@@ -35,10 +37,13 @@ for m in playlist:
     }
     olist.append(o)
 
-print(olist)
-
-with open("export_playlist.json","w") as o:
-    o.write(f'"{name}":{json.dumps(olist,indent=4)}')
+try:
+    with open("export_playlist.json","w") as o:
+        o.write(f'"{name}":{json.dumps(olist,indent=4)}')
+except Exception as e:
+    print("Something went wrong while trying to save your playlist.")
+    print(e)
+    sys.exit()
 
 print("Playlist exported! Check your active folder for the export_playlist.json file, and paste its contents into the songs.json file")
 input("Press any key to close")
